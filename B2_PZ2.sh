1d9445bc26821cd3504d5005de4eb9be19c98c5e
pilarik31 <pilarjan4111@gmail.com>
2018-04-05 10:12:03 +0200

    + B2_PZ1.sh + B2_PZ2.sh

---
+++ B2_PZ2.sh
@@ -0,0 +1,15 @@
+while [[ "$choice" != 4 ]]; do
+  echo "Zadej možnost:"
+  echo "1. možnost"
+  echo "2. možnost"
+  echo "3. možnost"
+  echo "4. konec"
+read choice
+
+case $choice in
+   1) echo "Já jsem první možnost." ;;
+   2) echo "Já jsem druhá možnost." ;;
+   3) echo "Já jsem třetí možnost." ;;
+   4) exit 0
+esac
+done
